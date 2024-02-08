be96f05512f1d7ef02ff2824837f9c3649c1d045
Molhamalfa <molham.business03@gmail.com>
2024-02-08 22:10:56 +0300

    spacer

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,11 @@
-import React from "react";
-import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
-import { Card } from "react-native-paper";
-import { SvgXml } from "react-native-svg";
-import star from "../../../../assets/star";
+import React from 'react';
+import styled from 'styled-components/native';
+import { Text, StyleSheet, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -22,34 +24,64 @@ const Title = styled(Text)`
 const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
-
 `;
 const Info = styled.View`
- padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
+`;
+const Rating = styled.View`
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+  flex-direction: row;
+`;
+const Section = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some Restaurant",
+    name = 'KFC',
     icon,
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
-    address = "100 some random street",
+    address = '100 some random street',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-       <Title>{name}</Title>
-       <SvgXml xml={star} width={20} height={20} />
-       <Address>{address}</Address>
-      </Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: 'red' }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+          </SectionEnd>
+        </Section>
 
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
-import styled from "styled-components/native";
+import React from 'react';
+import { Searchbar } from 'react-native-paper';
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
